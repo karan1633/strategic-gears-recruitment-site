ca30a2ac3e60e59eef399dbeff3275612c6603f9
Karan <123349027+karan1633@users.noreply.github.com>
2023-10-13 18:56:20 +0530

    changes in navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import Link from "next/link";
+import navStyles from "../styles/Navbar.module.css";
+import { useRef, useState } from "react";
+const Navbar = () => {
+  const imageStyle = {
+    width: "180px",
+    height: "62px !important",
+  };
+
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  const handleChangeOfTabIndex = (idx: number) => {
+    setActiveTabIndex(idx);
+  };
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link href="/candidates" className="">
+          <Image
+            src="/assets/images/SG_logo.svg"
+            width={100}
+            height={62}
+            alt="Picture of the author"
+            style={imageStyle}
+          />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse mx-5" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link
+              href="/candidates"
+              className={`nav-link mx-2 ${
+                activeTabIndex === 0 ? navStyles.active : navStyles.inactive
+              }`}
+              onClick={() => handleChangeOfTabIndex(0)}
+            >
+              <span className={``}>Candidates</span>
+            </Link>
+            <Link
+              href="/jobs"
+              className={`nav-link mx-2 ${
+                activeTabIndex === 1 ? navStyles.active : navStyles.inactive
+              }`}
+              onClick={() => handleChangeOfTabIndex(1)}
+            >
+              <span className={``}>Jobs</span>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        />
+        <script
+          src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+          async
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+          async
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
