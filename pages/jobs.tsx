9693c0449ff1375da097cf950ba0545ae2164354
Karan <123349027+karan1633@users.noreply.github.com>
2023-11-16 12:10:47 +0530

    fix: hydration err fixed :k

--- pages/jobs.tsx
+++ pages/jobs.tsx
@@ -1,7 +1,7 @@
 import Filter from '@/components/Candidates/Filter';
 import JobsListing from '@/components/Jobs/JobsListing';
 import { get_access_token } from '@/store/slices/token-slice';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 
@@ -9,28 +9,29 @@ const Jobs = () => {
   const router = useRouter();
 
   const { token }: any = useSelector(get_access_token);
+  const [content, setContent] = useState<JSX.Element | null>(null);
 
   useEffect(() => {
     if (token === '') {
       router.push('/login');
+    } else {
+      return setContent(
+        <div
+          className="row m-0"
+          style={{ backgroundColor: '#f8f9fa', height: '' }}
+        >
+          <div className="col-12 col-lg-2 mt-3">
+            <Filter />
+          </div>
+          <div className="col-12 col-lg-10 mt-3">
+            <JobsListing />
+          </div>
+        </div>
+      );
     }
   }, [token, router]);
 
-  if (token !== '') {
-    return (
-      <div
-        className="row m-0"
-        style={{ backgroundColor: '#f8f9fa', height: '' }}
-      >
-        <div className="col-12 col-lg-2 mt-3">
-          <Filter />
-        </div>
-        <div className="col-12 col-lg-10 mt-3">
-          <JobsListing />
-        </div>
-      </div>
-    );
-  }
+  return content;
 };
 
 export default Jobs;
